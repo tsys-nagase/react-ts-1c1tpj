31e9d622ce23ff2884ae8504bc9aa5d49e4a110d
tsys-nagase <yoshimasa_nagase@tsyscom.co.jp>
2023-04-29 13:26:03 +0900

    1 file modified

--- MyButton.tsx
+++ MyButton.tsx
@@ -4,16 +4,36 @@ import './style.css';
 
 export default function MyButton({ value }: { value: string }) {
   //console.log('🦠', value);
-  let [count, setCount] = useState(0);
-
+  const [count, setCount] = useState(0);
+  const [rCount, dispatch] = React.useReducer((prev, action) => {
+    if (action === '+') {
+      prev = prev + 1;
+    } else if (action === '-') {
+      prev = prev - 1;
+    }
+    return prev;
+  }, 0);
   const countUpHandler = () => {
-    console.log("countup")
-    setCount((prev) => prev+1);
+    console.log('countup');
+    setCount((prev) => prev + 1);
   };
+  const countUp = () => {
+    console.log('rHandler');
+    dispatch('+');
+  };
+  const countDown = () => {
+    dispatch('-');
+  };
+
   return (
     <div>
-      <div>{count}</div>
+      <div>state:{count}</div>
+
       <button onClick={countUpHandler}>ボタンです{value}</button>
+
+      <div>reducer:{rCount}</div>
+      <button onClick={countUp}>+</button>
+      <button onClick={countDown}>-</button>
     </div>
   );
 }
