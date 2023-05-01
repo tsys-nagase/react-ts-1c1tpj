0a4a6a1d1a9a374ab06d602f16df139928c30704
tsys-nagase <yoshimasa_nagase@tsyscom.co.jp>
2023-05-01 09:29:12 +0900

    1 file modified

--- App.tsx
+++ App.tsx
@@ -1,13 +1,19 @@
 import * as React from 'react';
+import Child from './Child';
 import MyButton from './MyButton';
 import './style.css';
 
+const context1 = React.createContext([]);
 export default function App() {
+  const [myState, setMyState] = React.useState({ value1: 'init' });
+
   return (
-    <div>
-      <h3>練習問題</h3>
-      <MyButton value="+" />
-      <Child />
-    </div>
+    <context1.Provider value={[myState, setMyState]}>
+      <div>
+        <h3>練習問題</h3>
+        <MyButton value="+" />
+        <Child />
+      </div>
+    </context1.Provider>
   );
 }
