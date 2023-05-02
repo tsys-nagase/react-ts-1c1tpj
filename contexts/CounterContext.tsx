17c5c2080bc445937980c178b4db5e6041904449
tsys-nagase <yoshimasa_nagase@tsyscom.co.jp>
2023-05-02 14:04:02 +0900

    1 file modified

--- contexts/CounterContext.tsx
+++ contexts/CounterContext.tsx
@@ -5,28 +5,33 @@ type CounterAction = {
   payload: { step: number; val1?: string };
 };
 
-const CounterContext = React.createContext<{
-  name: string;
+type CounterState = {
+  name?: string;
   num: number;
-}>({ name: 'def name', num: 0 });
+};
+
+const initialState: CounterState = { name: 'def name', num: 0 };
+
+const CounterContext = React.createContext<CounterState>(undefined);
 const CounterDispatchContext = React.createContext<
   React.Dispatch<CounterAction>
 >(() => {});
-
+// reducer
+export const counterReducer = (prev: CounterState, action: CounterAction) => {
+  console.log('step:', action.payload.step);
+  switch (action.type) {
+    case '+':
+      return { ...prev, num: prev.num + action.payload.step };
+    case '-':
+      return { ...prev, num: prev.num - action.payload.step };
+    default:
+      throw new Error('errorです');
+  }
+};
 export const CounterProvider = ({ children }) => {
   const [counter, counterDispatch] = React.useReducer(
-    (prev: number, action: CounterAction) => {
-      console.log('step:', action.payload.step);
-      switch (action.type) {
-        case '+':
-          return prev + action.payload.step;
-        case '-':
-          return prev - action.payload.step;
-        default:
-          throw new Error('errorです');
-      }
-    },
-    0
+    counterReducer,
+    initialState
   );
 
   return (
