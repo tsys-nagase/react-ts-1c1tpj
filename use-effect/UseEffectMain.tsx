bc2362adc33706f14cfbe74f3f402e067c9ba318
tsys-nagase <yoshimasa_nagase@tsyscom.co.jp>
2023-05-02 16:06:10 +0900

    1 file modified

--- use-effect/UseEffectMain.tsx
+++ use-effect/UseEffectMain.tsx
@@ -3,19 +3,24 @@ import { useEffect, useState } from 'react';
 
 export default function UseEffectMain() {
   const [time, setTime] = useState(0);
-
+  const [checked, setChecked] = useState(false);
   useEffect(() => {
     console.log('use Effect main called');
-    setInterval(() => {
-      setTime((prev) => {
-        return prev + 1;
-      });
-    }, 1000);
+    // setInterval(() => {
+    //   setTime((prev) => {
+    //     return prev + 1;
+    //   });
+    // }, 1000);
   }, []);
 
   useEffect(() => {
     console.log(time);
   }, [time]);
 
-  return <div style={{ background: '#f1f1f1' }}>UserEffectMain</div>;
+  return (
+    <div style={{ background: '#f1f1f1' }}>
+      UserEffectMain
+      <input type="checkbox" value={checked}></input>
+    </div>
+  );
 }
