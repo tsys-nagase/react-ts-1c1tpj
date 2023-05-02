d50eb7c8f78516d8a525281e063f5d97e0eedd42
tsys-nagase <yoshimasa_nagase@tsyscom.co.jp>
2023-05-02 16:00:51 +0900

    1 file modified

--- use-effect/UseEffectMain.tsx
+++ use-effect/UseEffectMain.tsx
@@ -1,11 +1,21 @@
+import * as React from 'react';
 import { useEffect, useState } from 'react';
 
 export default function UseEffectMain() {
   const [time, setTime] = useState(0);
+
   useEffect(() => {
     console.log('use Effect main called');
+    setInterval(() => {
+      setTime((prev) => {
+        return prev + 1;
+      });
+    }, 1000);
   }, []);
 
-  
+  useEffect(() => {
+    console.log(time);
+  }, [time]);
+
   return <div style={{ background: '#f1f1f1' }}>UserEffectMain</div>;
 }
